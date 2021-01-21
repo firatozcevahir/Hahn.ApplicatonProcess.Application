cf9be3620b41b8722f385b5db3645ff3eec3aa0b
Fırat Özcevahir <ozcevahirfi@TPSNBK06>
2021-01-21 18:50:36 +0300

    Update

--- Hahn-ApplicatonProcess-December2020-ClientApp/src/services/data-service.ts
+++ Hahn-ApplicatonProcess-December2020-ClientApp/src/services/data-service.ts
@@ -1,1 +1,16 @@
-// TODO : HTTP CLIENT INTEGRATION
+
+import { HttpClient } from 'aurelia-http-client'; // or 'aurelia-fetch-client' if you want to use fetch
+import { inject } from 'aurelia-framework';
+
+@inject(HttpClient)
+export class DataService {
+
+  public api = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '')
+  constructor(private http: HttpClient) {
+  }
+
+
+  public GetApplicants(): void {
+    this.http.get('')
+  }
+}
