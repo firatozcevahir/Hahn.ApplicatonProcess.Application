8ac25111624bc0e023c3c0ff530eac565f161ecb
firat.ozcevahir@gmail.com <firat.ozcevahir@gmail.com>
2021-01-22 00:29:32 +0300

    HttpClientTest

--- Hahn-ApplicatonProcess-December2020-ClientApp/src/app.ts
+++ Hahn-ApplicatonProcess-December2020-ClientApp/src/app.ts
@@ -1,19 +1,27 @@
 import { I18N } from 'aurelia-i18n';
 import routes from 'router';
 import { Router, RouterConfiguration } from 'aurelia-router';
+import { inject } from "aurelia-framework";
+import {  DataService } from './services/data-service';
 
+// @inject(DataService, I18N)
+@inject(DataService)
 export class App {
-  static inject = [I18N];
   public message = 'Hello World!';
   public router: Router;
 
-  constructor(private i18n: I18N) {
-    this.i18n = i18n;
-    this.i18n
-      .setLocale('en')
-      .then(() => {
-        console.log('locale is set');
-      });
+  constructor(
+    // private i18n: I18N,
+    private dataService: DataService
+    ) {
+    // this.i18n = i18n;
+    // this.i18n
+    //   .setLocale('en')
+    //   .then(() => {
+    //     console.log('locale is set');
+    //   });
+
+      this.dataService.GetApplicants();
   }
 
   public configureRouter(config: RouterConfiguration, router: Router): void {
--- Hahn-ApplicatonProcess-December2020-ClientApp/src/services/data-service.ts
+++ Hahn-ApplicatonProcess-December2020-ClientApp/src/services/data-service.ts
@@ -1,16 +1,18 @@
-
-import { HttpClient } from 'aurelia-http-client'; // or 'aurelia-fetch-client' if you want to use fetch
-import { inject } from 'aurelia-framework';
+import { HttpClient } from "aurelia-http-client"; // or 'aurelia-fetch-client' if you want to use fetch
+import { inject } from "aurelia-framework";
 
 @inject(HttpClient)
 export class DataService {
-
-  public api = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '')
-  constructor(private http: HttpClient) {
-  }
-
+  public api =
+    location.protocol +
+    "//" +
+    location.hostname +
+    (location.port ? ":" + location.port : "");
+  constructor(private http: HttpClient) {}
 
   public GetApplicants(): void {
-    this.http.get('')
+    this.http.get("http://localhost:7000/app/applicant/get").then((res) => {
+      console.log(res);
+    });
   }
 }
